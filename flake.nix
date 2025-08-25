6e6d165820c2dabf30024527119fc7ee7ce9fe0e
clarelab <yaho@clarelab.moe>
2025-08-25 13:43:33 +0200

    init

---
+++ flake.nix
@@ -0,0 +1,37 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+
+        erlang  = pkgs.erlang_26;
+        elixir  = pkgs.beam.packages.erlang_26.elixir_1_16;
+        psql    = pkgs.postgresql_16;
+
+        devPkgs = [ pkgs.bashInteractive pkgs.coreutils pkgs.git pkgs.curl pkgs.openssl erlang elixir psql ];
+        rtPkgs  = [ pkgs.bash pkgs.coreutils pkgs.openssl erlang ];
+
+        mkImage = { name, tag, contents, workdir, cmd ? null }:
+          pkgs.dockerTools.buildLayeredImage {
+            inherit name tag contents;
+            config = {
+              WorkingDir = workdir;
+              Env = [ "LANG=C.UTF-8" "LC_ALL=C.UTF-8" "HOME=/root" "PATH=/bin:/usr/bin:/sbin:/usr/sbin" ];
+              Cmd = if cmd == null then null else cmd;
+            };
+          };
+      in {
+        devShells.default = pkgs.mkShell { packages = devPkgs; };
+
+        packages.dev      = mkImage { name="api-base"; tag="dev";      contents=devPkgs; workdir="/workspace"; cmd=[ "sleep" "infinity" ]; };
+        packages.runtime  = mkImage { name="api-base"; tag="runtime";  contents=rtPkgs;  workdir="/app";       };
+
+        packages.default = self.packages.${system}.dev;
+      }
+    );
+}
