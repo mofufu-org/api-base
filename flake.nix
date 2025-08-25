5aa085d8b9a0a72dc980a73323e7808641d41d28
clarelab <yaho@clarelab.moe>
2025-08-26 01:17:56 +0200

    hotfix

--- flake.nix
+++ flake.nix
@@ -23,7 +23,6 @@
         devPkgs   = commonPkgs ++ [
           pkgs.git
           pkgs.curl
-          pkgs.cacert
           pkgs.busybox
         ];
         rtPkgs    = commonPkgs;
@@ -76,6 +75,7 @@
               paths       = devPkgs;
               pathsToLink = [ "/bin" ];
             })
+            pkgs.cacerts
           ];
           config = {
             WorkingDir = "/workspace";
