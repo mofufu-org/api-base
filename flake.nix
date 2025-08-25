16ed75d17a81b59b107131cff9611bb7650e25d5
clarelab <yaho@clarelab.moe>
2025-08-25 21:00:56 +0200

    hotfix

--- flake.nix
+++ flake.nix
@@ -86,7 +86,7 @@
           workdir  = "/workspace";
           cmd      = [
             "code-server"
-            "--auth none"
+            "--auth \"none\""
             "--cert /etc/ssl/certs/ca-bundle.crt"
             "--bind-addr 127.0.0.1:8080"
             "--extensions-dir /root/.vscode-oss/extensions"
