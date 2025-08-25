d91493a27a562f08bc27f489157573e04ee09a3f
clarelab <yaho@clarelab.moe>
2025-08-25 23:39:10 +0200

    hotfix

--- flake.nix
+++ flake.nix
@@ -61,10 +61,11 @@
           name = "api-base";
           tag  = "dev";
           fromImage = pkgs.dockerTools.pullImage {
-            imageName = "debian";
-            imageTag = "bookworm-slim";
-            imageDigest = "sha256:b1a741487078b369e78119849663d7f1a5341ef2768798f7b7406c4240f86aef";
-            sha256 = "sha256-GsiMvKEcc1SbPNJubtU0xFNBbno5PMiQY9pxKRcbeK0=";
+            imageName      = "debian";
+            imageDigest    = "sha256:b1a741487078b369e78119849663d7f1a5341ef2768798f7b7406c4240f86aef";
+            hash           = "sha256-GsiMvKEcc1SbPNJubtU0xFNBbno5PMiQY9pxKRcbeK0=";
+            finalImageName = "debian";
+            finalImageTag  = "bookworm-slim";
           };
           contents = devPkgs;
           config = {
